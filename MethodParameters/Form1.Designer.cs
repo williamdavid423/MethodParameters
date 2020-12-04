036c67210edbeaa2958d57e13f656aacd608aca2
Tony Theodoropoulos <theodot@outlook.com>
2020-12-04 09:24:48 -0500

    Add project files.

---
+++ MethodParameters/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿
+namespace MethodParameters
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.q3Output = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pinInput = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.q1Output = new System.Windows.Forms.Label();
+            this.num1Label = new System.Windows.Forms.Label();
+            this.q1Num1Input = new System.Windows.Forms.TextBox();
+            this.num2Label = new System.Windows.Forms.Label();
+            this.q1Num2Input = new System.Windows.Forms.TextBox();
+            this.q2Output = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.ageInput = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.testButton = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.q4Output = new System.Windows.Forms.Label();
+            this.inchesLabel = new System.Windows.Forms.Label();
+            this.inchesInput = new System.Windows.Forms.TextBox();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.userInput);
+            this.groupBox3.Controls.Add(this.q3Output);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.pinInput);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.ForeColor = System.Drawing.Color.White;
+            this.groupBox3.Location = new System.Drawing.Point(236, 67);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(194, 145);
+            this.groupBox3.TabIndex = 31;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Question 3";
+            // 
+            // userInput
+            // 
+            this.userInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userInput.Location = new System.Drawing.Point(88, 32);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(99, 24);
+            this.userInput.TabIndex = 13;
+            this.userInput.Text = "0";
+            // 
+            // q3Output
+            // 
+            this.q3Output.BackColor = System.Drawing.Color.White;
+            this.q3Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q3Output.ForeColor = System.Drawing.Color.Black;
+            this.q3Output.Location = new System.Drawing.Point(8, 98);
+            this.q3Output.Name = "q3Output";
+            this.q3Output.Size = new System.Drawing.Size(179, 33);
+            this.q3Output.TabIndex = 11;
+            this.q3Output.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 18);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Username:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 18);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "PIN:";
+            // 
+            // pinInput
+            // 
+            this.pinInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pinInput.Location = new System.Drawing.Point(87, 58);
+            this.pinInput.Name = "pinInput";
+            this.pinInput.Size = new System.Drawing.Size(100, 24);
+            this.pinInput.TabIndex = 15;
+            this.pinInput.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.q1Output);
+            this.groupBox1.Controls.Add(this.num1Label);
+            this.groupBox1.Controls.Add(this.q1Num1Input);
+            this.groupBox1.Controls.Add(this.num2Label);
+            this.groupBox1.Controls.Add(this.q1Num2Input);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(15, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 145);
+            this.groupBox1.TabIndex = 28;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Question 1";
+            // 
+            // q1Output
+            // 
+            this.q1Output.BackColor = System.Drawing.Color.White;
+            this.q1Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q1Output.ForeColor = System.Drawing.Color.Black;
+            this.q1Output.Location = new System.Drawing.Point(7, 98);
+            this.q1Output.Name = "q1Output";
+            this.q1Output.Size = new System.Drawing.Size(179, 33);
+            this.q1Output.TabIndex = 3;
+            this.q1Output.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // num1Label
+            // 
+            this.num1Label.AutoSize = true;
+            this.num1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1Label.Location = new System.Drawing.Point(64, 35);
+            this.num1Label.Name = "num1Label";
+            this.num1Label.Size = new System.Drawing.Size(56, 18);
+            this.num1Label.TabIndex = 4;
+            this.num1Label.Text = "Num 1:";
+            // 
+            // q1Num1Input
+            // 
+            this.q1Num1Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q1Num1Input.Location = new System.Drawing.Point(123, 32);
+            this.q1Num1Input.Name = "q1Num1Input";
+            this.q1Num1Input.Size = new System.Drawing.Size(62, 24);
+            this.q1Num1Input.TabIndex = 5;
+            this.q1Num1Input.Text = "0";
+            // 
+            // num2Label
+            // 
+            this.num2Label.AutoSize = true;
+            this.num2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2Label.Location = new System.Drawing.Point(64, 58);
+            this.num2Label.Name = "num2Label";
+            this.num2Label.Size = new System.Drawing.Size(56, 18);
+            this.num2Label.TabIndex = 6;
+            this.num2Label.Text = "Num 2:";
+            // 
+            // q1Num2Input
+            // 
+            this.q1Num2Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q1Num2Input.Location = new System.Drawing.Point(123, 58);
+            this.q1Num2Input.Name = "q1Num2Input";
+            this.q1Num2Input.Size = new System.Drawing.Size(62, 24);
+            this.q1Num2Input.TabIndex = 7;
+            this.q1Num2Input.Text = "0";
+            // 
+            // q2Output
+            // 
+            this.q2Output.BackColor = System.Drawing.Color.White;
+            this.q2Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q2Output.ForeColor = System.Drawing.Color.Black;
+            this.q2Output.Location = new System.Drawing.Point(8, 75);
+            this.q2Output.Name = "q2Output";
+            this.q2Output.Size = new System.Drawing.Size(179, 42);
+            this.q2Output.TabIndex = 8;
+            this.q2Output.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Location = new System.Drawing.Point(76, 37);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(44, 20);
+            this.ageLabel.TabIndex = 9;
+            this.ageLabel.Text = "age:";
+            // 
+            // ageInput
+            // 
+            this.ageInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageInput.Location = new System.Drawing.Point(123, 35);
+            this.ageInput.Name = "ageInput";
+            this.ageInput.Size = new System.Drawing.Size(65, 24);
+            this.ageInput.TabIndex = 10;
+            this.ageInput.Text = "0";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.q2Output);
+            this.groupBox2.Controls.Add(this.ageLabel);
+            this.groupBox2.Controls.Add(this.ageInput);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(15, 226);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(194, 131);
+            this.groupBox2.TabIndex = 30;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Question 2";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-3, -2);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(453, 52);
+            this.titleLabel.TabIndex = 29;
+            this.titleLabel.Text = "Method Parameters";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // testButton
+            // 
+            this.testButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGoldenrod;
+            this.testButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.testButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testButton.ForeColor = System.Drawing.Color.White;
+            this.testButton.Location = new System.Drawing.Point(236, 386);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(194, 36);
+            this.testButton.TabIndex = 25;
+            this.testButton.Text = "Test";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.q4Output);
+            this.groupBox4.Controls.Add(this.inchesLabel);
+            this.groupBox4.Controls.Add(this.inchesInput);
+            this.groupBox4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.ForeColor = System.Drawing.Color.White;
+            this.groupBox4.Location = new System.Drawing.Point(236, 226);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(194, 131);
+            this.groupBox4.TabIndex = 32;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Question 4";
+            // 
+            // q4Output
+            // 
+            this.q4Output.BackColor = System.Drawing.Color.White;
+            this.q4Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q4Output.ForeColor = System.Drawing.Color.Black;
+            this.q4Output.Location = new System.Drawing.Point(8, 75);
+            this.q4Output.Name = "q4Output";
+            this.q4Output.Size = new System.Drawing.Size(179, 42);
+            this.q4Output.TabIndex = 8;
+            this.q4Output.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // inchesLabel
+            // 
+            this.inchesLabel.AutoSize = true;
+            this.inchesLabel.Location = new System.Drawing.Point(57, 37);
+            this.inchesLabel.Name = "inchesLabel";
+            this.inchesLabel.Size = new System.Drawing.Size(66, 20);
+            this.inchesLabel.TabIndex = 9;
+            this.inchesLabel.Text = "inches:";
+            // 
+            // inchesInput
+            // 
+            this.inchesInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inchesInput.Location = new System.Drawing.Point(126, 35);
+            this.inchesInput.Name = "inchesInput";
+            this.inchesInput.Size = new System.Drawing.Size(62, 24);
+            this.inchesInput.TabIndex = 10;
+            this.inchesInput.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(445, 443);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.testButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Method Parameters";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.Label q3Output;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pinInput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label q1Output;
+        private System.Windows.Forms.Label num1Label;
+        private System.Windows.Forms.TextBox q1Num1Input;
+        private System.Windows.Forms.Label num2Label;
+        private System.Windows.Forms.TextBox q1Num2Input;
+        private System.Windows.Forms.Label q2Output;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.TextBox ageInput;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button testButton;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label q4Output;
+        private System.Windows.Forms.Label inchesLabel;
+        private System.Windows.Forms.TextBox inchesInput;
+    }
+}
+
---
+++ MethodParameters/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MethodParameters
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            //get values for question 1 and send them to the Add method
+            int num1 = Convert.ToInt16(q1Num1Input.Text);
+            int num2 = Convert.ToInt16(q1Num2Input.Text);
+            Add(num1, num2);
+
+            //get value for question 2 and send it to the CanVote method
+            int age = Convert.ToInt16(ageInput.Text);
+            CanVote(age);
+
+            //get values for question 3 and send them to the WelcomeUser method
+
+
+            //get value for question 4 and send it to the InchToCm method
+
+
+        }
+
+        // 1. Create a method called Add, that accepts 2 int parameters, x and y,
+        //    adds them together, and then displays the result.
+        //
+        //    Sample input and output:
+        //
+        //    Input: 4, 5
+        //    4 + 5 = 9
+        //
+        //    Input: 6, 1
+        //    6 + 1 = 7;
+
+        public void Add(int x, int y)
+        {
+
+        }
+
+        // 2. Create a method called CanVote, that accepts an int parameter, 
+        //    voterAge, and uses it to determine if the user is old enough to vote.
+        //    
+        //    Sample input and output:
+        //
+        //    Input: 19
+        //    You are eligible to vote
+        //
+        //    Input: 15
+        //    You are not eligible to vote
+
+        public void CanVote(int voterAge)
+        {
+
+        }
+
+        // 3. Create a method called WelcomeUser, that accepts 2 string 
+        //    parameters, name and pin. If the PIN is 1234 the program welcomes
+        //    the user. If not an error message is displayed.
+        //    
+        //    Sample input and output:
+        //
+        //    Input: Mr. T, 1111
+        //    Incorrect PIN
+        //
+        //    Input: Mr. T, 1234
+        //    Welcome Mr. T
+
+
+
+        // 4. Create a method called InchToCm, that accepts a double 
+        //    parameter, inches, converts it to cm, (*2.54), and displays
+        //    the result.
+        //
+        //    Sample input and output:
+        //
+        //    Input: 2
+        //    2 inches is 5.08 cms
+        //
+        //    Input: 3.4
+        //    3.4 inches is 8.636 cms
+
+
+
+    }
+}
